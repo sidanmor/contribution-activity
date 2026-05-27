c562a2eea2a955765dc287805e05ecd844019a85
Idan Mor <sidanmor@gmail.com>
2026-05-27 16:25:41 +0300

    Fix hidden runner bash argument passing

--- run-contribution-activity-hidden.ps1
+++ run-contribution-activity-hidden.ps1
@@ -3,15 +3,19 @@ $ErrorActionPreference = 'Stop'
 $bashPath = 'C:\Program Files\Git\bin\bash.exe'
 $workingDir = 'C:\Repos\idan\contribution-activity'
 $bashCommand = 'cd /c/Repos/idan/contribution-activity && chmod +x contribution-activity.sh && ./contribution-activity.sh'
+$stdoutLog = 'C:\Repos\idan\contribution-activity\task-runner.out.log'
+$stderrLog = 'C:\Repos\idan\contribution-activity\task-runner.err.log'
 
 if (-not (Test-Path $bashPath)) {
     throw "Git Bash not found at $bashPath"
 }
 
 $process = Start-Process -FilePath $bashPath `
-    -ArgumentList @('-lc', $bashCommand) `
+    -ArgumentList "-lc \"$bashCommand\"" `
     -WorkingDirectory $workingDir `
     -WindowStyle Hidden `
+    -RedirectStandardOutput $stdoutLog `
+    -RedirectStandardError $stderrLog `
     -PassThru `
     -Wait
 
