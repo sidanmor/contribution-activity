84fbe5ab45f3ce0587a469f364ece0502d49774a
Idan Mor <sidanmor@gmail.com>
2026-05-27 13:28:12 +0300

    Add hidden runner script for scheduled task

---
+++ run-contribution-activity-hidden.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop'
+
+$bashPath = 'C:\Program Files\Git\bin\bash.exe'
+$workingDir = 'C:\Repos\idan\contribution-activity'
+$bashCommand = 'cd /c/Repos/idan/contribution-activity && chmod +x contribution-activity.sh && ./contribution-activity.sh'
+
+if (-not (Test-Path $bashPath)) {
+    throw "Git Bash not found at $bashPath"
+}
+
+$process = Start-Process -FilePath $bashPath \
+    -ArgumentList @('-lc', $bashCommand) \
+    -WorkingDirectory $workingDir \
+    -WindowStyle Hidden \
+    -PassThru \
+    -Wait
+
+exit $process.ExitCode
